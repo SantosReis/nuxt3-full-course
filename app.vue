5976b6dd0e49a336a84cbd113d12046d1868a02c
Marcus Reis <mvsantosreis@gmail.com>
2024-04-30 17:58:51 +0100

    Dynamic components ex2

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
-  <component :is="Counter"></component>
+  <button @click="toggle">Toggle</button>
+  <div v-if="MyComponent === 'welcome'">
+    <HomeWelcome />
+  </div>
+  <Counter v-else />
 </template>
 <script setup>
-const Counter = resolveComponent("Counter");
+// const MyComponent = shallowRef("Counter");
+const MyComponent = ref("counter");
+
+function toggle() {
+  MyComponent.value = "welcome";
+}
 </script>
