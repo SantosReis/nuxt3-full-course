2d1104593766bcdf5ce28f9521ef87dd59c30a36
Marcus Reis <mvsantosreis@gmail.com>
2024-04-30 17:28:37 +0100

    Hello world

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div>Hello world</div>
+  <button @click="increment">Increment</button>
+  <p>{{ count }}</p>
+  <button @click="decrement">Decrement</button>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+};
+</script>
+<script setup>
+// const count = ref(0);
+// function increment() {
+//   count.value++;
+// }
+// function decrement() {
+//   count.value--;
+// }
+</script>
