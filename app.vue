c173c0c342272708cda407e73b0787cec8bf4fad
Marcus Reis <mvsantosreis@gmail.com>
2024-04-30 17:34:48 +0100

    Hello World as Component

--- app.vue
+++ app.vue
@@ -1,32 +1,4 @@
 <template>
   <div>Hello world</div>
-  <button @click="increment">Increment</button>
-  <p>{{ count }}</p>
-  <button @click="decrement">Decrement</button>
+  <Counter />
 </template>
-<script>
-export default {
-  data() {
-    return {
-      count: 0,
-    };
-  },
-  methods: {
-    increment() {
-      this.count++;
-    },
-    decrement() {
-      this.count--;
-    },
-  },
-};
-</script>
-<script setup>
-// const count = ref(0);
-// function increment() {
-//   count.value++;
-// }
-// function decrement() {
-//   count.value--;
-// }
-</script>
